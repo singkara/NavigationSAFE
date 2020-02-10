f55df41773cae4b49c9f5ef4b4de3fcea0044ee1
Karan Deep Singh <karan100194@gmail.com>
2020-02-10 17:52:59 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+declare var H: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'here-project';
+export class AppComponent implements OnInit {
+
+    private platform: any;
+
+    @ViewChild("map")
+    public mapElement: ElementRef;
+
+    public constructor() {
+        this.platform = new H.service.Platform({
+            "app_id": "APP-ID-HERE",
+            "app_code": "APP-CODE-HERE"
+        });
+    }
+
+    public ngOnInit() { }
+
+    public ngAfterViewInit() {
+        let defaultLayers = this.platform.createDefaultLayers();
+        let map = new H.Map(
+            this.mapElement.nativeElement,
+            defaultLayers.normal.map,
+            {
+                zoom: 10,
+                center: { lat: 37.7397, lng: -121.4252 }
+            }
+        );
+    }
+
 }
